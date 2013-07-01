d49040f46f181857fd37304616f2c8943888e443
Tobi <tidowu93@yahoo.com>
2013-07-01 19:07:50 +0300

    Finish Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -22,6 +22,7 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
+    
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
@@ -35,10 +36,25 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
+    
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
